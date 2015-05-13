eeb26188344b976e28e253c5ae82bc9635ea7c16
sahidr <sahidr93@gmail.com>
2015-05-13 15:10:38 -0430

    Commit inicial para el repositorio. Funciona Login.

---
+++ app/scrum/login.py
@@ -0,0 +1,70 @@
+'''
+Created on 30/4/2015
+'''
+from   app.scrum.model import *
+import uuid
+import hashlib
+import re
+
+class clsLogin(object):
+    '''Permite manejar la seguridad de una clave'''
+    
+    def __init__(self):
+        ohast=''
+        self.expressionRegular = ('(([a-z]|[A-Z]|\d|[@.#$+*])*[@.#$+*]([a-z]|[A-Z]|\d|[@.#$+*])*[A-Z]([a-z]|[A-Z]|\d|[@.#$+*])*\d([a-z]|[A-Z]|\d|[@.#$+*])*)|'
+                                  '(([a-z]|[A-Z]|\d|[@.#$+*])*[@.#$+*]([a-z]|[A-Z]|\d|[@.#$+*])*\d([a-z]|[A-Z]|\d|[@.#$+*])*[A-Z]([a-z]|[A-Z]|\d|[@.#$+*])*)|'
+                                  '(([a-z]|[A-Z]|\d|[@.#$+*])*[A-Z]([a-z]|[A-Z]|\d|[@.#$+*])*[@.#$+*]([a-z]|[A-Z]|\d|[@.#$+*])*\d([a-z]|[A-Z]|\d|[@.#$+*])*)|'
+                                  '(([a-z]|[A-Z]|\d|[@.#$+*])*[A-Z]([a-z]|[A-Z]|\d|[@.#$+*])*\d([a-z]|[A-Z]|\d|[@.#$+*])*[@.#$+*]([a-z]|[A-Z]|\d|[@.#$+*])*)|'
+                                  '(([a-z]|[A-Z]|\d|[@.#$+*])*\d([a-z]|[A-Z]|\d|[@.#$+*])*[@.#$+*]([a-z]|[A-Z]|\d|[@.#$+*])*[A-Z]([a-z]|[A-Z]|\d|[@.#$+*])*)|'
+                                  '(([a-z]|[A-Z]|\d|[@.#$+*])*\d([a-z]|[A-Z]|\d|[@.#$+*])*[A-Z]([a-z]|[A-Z]|\d|[@.#$+*])*[@.#$+*]([a-z]|[A-Z]|\d|[@.#$+*])*)')
+   
+    def validPassword(self, password):
+        olength_password=self.length_password(password)        
+        if olength_password>=8 and olength_password<=16:
+            validPassword = re.search(self.expressionRegular,password) 
+            return validPassword
+
+
+
+    def encript(self, value):
+        '''Permite encriptar una clave dada'''
+        # Verificar la longitud del password
+        oHash=""
+        olength_password=self.length_password(value)
+        if olength_password>=8 and olength_password<=16:
+            validPassword= re.search(self.expressionRegular, value)
+            if validPassword:
+            # uuid es usado para generar numeros random
+                salt = uuid.uuid4().hex
+            # hash
+                oHash= hashlib.sha256(salt.encode() + value.encode()).hexdigest() + ':' + salt
+                return oHash
+            else:
+                #print('El password no posee los caracteres correspondientes')
+                return oHash
+        else:
+            #print('El Password debe contener entre 8 y 16 caracteres')
+            return oHash   
+    
+    def check_password(self, oPassworkEncript, oCheckPassword):
+        '''Permite comprobar si una clave encriptada corresponde a una clave dada'''
+        # Verificar la longitud del password
+        olength_password=self.length_password(oCheckPassword)        
+        if olength_password>=8 and olength_password<=16:
+            validPassword= re.search(self.expressionRegular,oCheckPassword) 
+            if validPassword:
+                # uuid es usado para generar numeros random
+                oPassworkEncript, salt = oPassworkEncript.split(':')
+                return oPassworkEncript == hashlib.sha256(salt.encode() + oCheckPassword.encode()).hexdigest()
+            else:
+                #print('El password no posee los caracteres correspondientes')
+                return False
+        else:
+            #print('El Password no posee la cantidad de caracteres requerida')
+            return False
+    
+    def length_password(self, user_password):
+        '''Permite verificar el tamano de un password'''
+        # uuid es usado para generar numeros random
+        return len(user_password)
+
---
+++ app/scrum/model.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-. 
+
+# Se importan las librerias necesarias.
+import os
+import sys
+from flask                 import Flask
+from flask.ext.migrate     import Migrate, MigrateCommand
+from flask.ext.sqlalchemy  import SQLAlchemy
+from flask.ext.script      import Manager
+#from sqlalchemy.sql.schema import CheckConstraint
+
+
+# Conexion con la base de datos.
+basedir                 = os.path.abspath(os.path.dirname(__file__))
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'apl.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+
+# Instancia de la aplicacion.
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+	'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+
+# Instancia de la base de datos. 
+db = SQLAlchemy(app)
+
+# Descripcion de la Base de Datos.
+
+# Declaracion del modelo Role
+class clsRole(db.Model):
+    '''Clase que define el modelo Role'''
+
+    __tablename__ = 'roles'
+    idrole    = db.Column(db.Integer, primary_key=True)
+    namerole  = db.Column(db.String(50), unique=True)
+    user_role = db.relationship('clsUser',backref='role',lazy = 'dynamic',cascade = "all, delete, delete-orphan")
+    #CheckConstraint(namerole in ('Product Owner','Scrum Master', 'Team member'), name='check_namerole')
+      
+   
+    def __init__(self, idrole, namerole):
+        '''Constructor del modelo Role'''
+        self.idrole   = idrole
+        self.namerole = namerole
+
+    def __repr__(self):
+        '''Respresentacion en string del nombre del Role'''
+        return '<Role %r>' % self.namerole
+
+
+class clsUser(db.Model):
+    '''Clase que define el modelo Usuario'''
+ 
+    __tablename__ = 'user'
+    fullname = db.Column(db.String(50))
+    username = db.Column(db.String(16), primary_key = True, index = True)
+    password = db.Column(db.String(200))
+    email    = db.Column(db.String(30), unique = True )
+    id_role  = db.Column(db.Integer, db.ForeignKey('roles.idrole'))
+
+    def __init__(self, fullname, username, password, email, idrole):
+        '''Constructor del modelo usuario'''
+        self.fullname = fullname
+        self.username = username
+        self.password = password
+        self.email    = email
+        self.idrole   = idrole
+   
+    def __repr__(self):
+        '''Representacion en string del nombre de Usuario'''
+        return '<username %r, email %r>' % (self.username, self.email)
+
+migrate = Migrate(app, db)
+manager = Manager(app)
+
+manager.add_command('db', MigrateCommand)
+
+#db.drop_all()   # Borramos la base de datos
+db.create_all() # Creamos la base de datos
+
