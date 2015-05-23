6c2a4c5ff547fcc531fd84f8ffa28617299fda8d
plantijn <cplantijn@gmail.com>
2015-05-23 03:27:32 -0430

    Varias modificaciones en la conexión entre las vistas del modulo de Objetivos y la base de datos

--- app/scrum/objetivo.py
+++ app/scrum/objetivo.py
@@ -1,7 +1,22 @@
 # -*- coding: utf-8 -*-
-from flask import request, session, Blueprint, json
 
+#Agregando proyect root
+
+import sys
+import os
+dir = os.path.abspath(os.path.join(os.path.abspath(__file__), '../../..'))
+sys.path.append(dir)
+
+#Dependencias flask
+from flask import request, session, Blueprint, json
+from sqlalchemy import create_engine
+from sqlalchemy.engine.url import URL
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql.expression import text
+
+#Definicion de blueprint y bd
 objetivo = Blueprint('objetivo', __name__)
+from base import *
 
 
 @objetivo.route('/objetivo/ACrearObjetivo', methods=['POST'])
@@ -10,14 +25,15 @@ def ACrearObjetivo():
     params = request.get_json()
     results = [{'label':'/VProducto', 'msg':['Objetivo creado']}, {'label':'/VCrearObjetivo', 'msg':['Error al crear objetivo']}, ]
     res = results[0]
-    #Action code goes here, res should be a list with a label and a message
     
-    #newDescription = params
-    #oObject = clsObjective()
-    idPila = 1
+    #Action code goes here, res should be a list with a label and a message  
+    idPila = str(session['idPila'])
+    session.pop('idPila', None)
+    objetivo = objective()
+    objetivo.insertObjective(descObjective = params['descripcion'], id_backlog = idPila)
     res['label'] = res['label'] + '/' + str(idPila)
-
     #Action code ends here
+    
     if "actor" in res:
         if res['actor'] is None:
             session.pop("actor", None)
@@ -33,12 +49,14 @@ def AModifObjetivo():
     params = request.get_json()
     results = [{'label':'/VProducto', 'msg':['Objetivo actualizado']}, {'label':'/VObjetivo', 'msg':['Error al modificar objetivo']}, ]
     res = results[0]
+    
     #Action code goes here, res should be a list with a label and a message
-
-    idPila = 1
+    idPila = int(request.args.get('idPila', 1))
+    objetivo = objective()
+    objetivo.updateObjective(params['descripcion'])
     res['label'] = res['label'] + '/' + str(idPila)
-
     #Action code ends here
+    
     if "actor" in res:
         if res['actor'] is None:
             session.pop("actor", None)
@@ -53,10 +71,12 @@ def VCrearObjetivo():
     res = {}
     if "actor" in session:
         res['actor']=session['actor']
+        
     #Action code goes here, res should be a JSON structure
-
-
+    params = request.get_json()    
+    session['idPila'] = request.args['idPila']
     #Action code ends here
+    
     return json.dumps(res)
 
 
@@ -66,11 +86,19 @@ def VObjetivo():
     res = {}
     if "actor" in session:
         res['actor']=session['actor']
-    #Action code goes here, res should be a JSON structure
+        
+    #Action code goes here, res should be a JSON structure     
+    objetivo = objective()
 
-    res['idPila'] = 1 
+    idPila = int(request.args.get('idPila', 1))
+    #objs = objetivo.listarObjetivos()
+    #res['fObjetivo'] = objs[idProducto-1]
+    #idObjetivo=idPila
+
+    #res['idPila'] = 1 
 
     #Action code ends here
+    
     return json.dumps(res)
 
 
