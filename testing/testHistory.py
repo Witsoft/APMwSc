7bd2ae8964e3d752c4a43cd18dda9a8f82243258
patriv <patty.vl91@gmail.com>
2015-05-25 10:42:15 -0430

    Se creó la función "searchUserHistory" con sus respectivos casos de prueba.

--- testing/testHistory.py
+++ testing/testHistory.py
@@ -139,6 +139,77 @@ class TestHistory(unittest.TestCase):
         aBackLog.deleteProduct('Taxi seguro.')
         
         
+ #############################################      
+    #   Suite de Pruebas para searchUserHistory #
+    #############################################
+    
+    #Casos Frontera
+    
+    #Caso 1
+    def testSearchHistoryExist(self):
+        aBackLog = backLog()
+        aBackLog.insertBackLog('Taxi seguro.','Permite localizar un taxi')
+        userH = userHistory()
+        userH.insertUserHistory('codigo1','obligatorio',1)
+        result = userH.searchUserHistory('codigo1')
+        aBackLog.deleteProduct('Reservar un taxi.')        
+    
+    #Caso 2
+    def testSearchHistoryTrue(self):
+        aBackLog = backLog()
+        aBackLog.insertBackLog('Taxi seguro.','Permite localizar un taxi')
+        userH = userHistory()
+        userH.insertUserHistory('codigo2','obligatorio',1)
+        result = userH.searchUserHistory('codigo2')
+        self.assertNotEqual([],result)
+        #userH.deleteUserHistory('codigo2')
+        aBackLog.deleteProduct('Reservar un taxi.')
+    
+    #Caso 3
+    def testSearchHistoryNotExist(self):
+        aBackLog = backLog()
+        aBackLog.insertBackLog('Taxi seguro.','Permite localizar un taxi')
+        userH = userHistory()
+        userH.insertUserHistory('codigo3','obligatorio',1)
+        result = userH.searchUserHistory('codigo4')
+        self.assertEqual([],result)
+        #userH.deleteUserHistory('codigo3')
+        aBackLog.deleteProduct('Reservar un taxi.')
+    
+    #Caso 4
+    def testSearchHistoryLong10(self):
+        aBackLog = backLog()
+        aBackLog.insertBackLog('Taxi seguro.','Permite localizar un taxi')
+        userH = userHistory()
+        userH.insertUserHistory('codigo7890','obligatorio',1)
+        result = userH.searchUserHistory('codigo7890')
+        self.assertNotEqual([],result)
+        #userH.deleteUserHistory('codigo7890')
+        aBackLog.deleteProduct('Reservar un taxi.')
+    
+    #Caso 5    
+    def testSearchHistoryLong11(self):
+        aBackLog = backLog()
+        aBackLog.insertBackLog('Taxi seguro.','Permite localizar un taxi')
+        userH = userHistory()
+        userH.insertUserHistory('codigo78910','obligatorio',1)
+        result = userH.searchUserHistory('codigo78910')
+        self.assertNotEqual([],result)
+        aBackLog.deleteProduct('Reservar un taxi.')
+    
+    #Casos Malicia
+    
+    #Caso 6
+    def testSearchHistoryNone(self):
+        aBackLog = backLog()
+        aBackLog.insertBackLog('Taxi seguro.','Permite localizar un taxi')
+        userH = userHistory()
+        userH.insertUserHistory(None,'obligatorio',1)
+        result = userH.searchUserHistory(None)
+        self.assertEqual([],result)
+        aBackLog.deleteProduct('Reservar un taxi.') 
+        
+        
     ######################################################      
     #       Suite de Pruebas para deleteUserHistory      #
     ###################################################### 
