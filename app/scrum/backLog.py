3717626b9cd7f9160558c865b843580e5d7f7a6c
patriv <patty.vl91@gmail.com>
2015-05-25 15:03:49 -0430

    Corrección atributo description en la clase backlog.

--- app/scrum/backLog.py
+++ app/scrum/backLog.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-. 
 
 from app.scrum.model import *
+from lxml.html._diffcommand import description
 
 # Declaracion de constantes.
 const_maxDescription = 140
@@ -44,59 +45,38 @@ class backLog(object):
                     db.session.commit()
                     return True
 
-    def modifyDescription(self, dName, new_dName):   
+    def modifyDescription(self, description, new_description):   
         '''Permite actualizar los valores de una Descripcion'''    
-        if ((type(new_dName) != str) or (type(dName) != str)):
-            return False
-        else:
-            aName = self.findName(dName)
-            if (aName == []):
-                return False
-            else:
-                new_dName = clsBackLog(BL_name = new_dName)
-                long_dName = len(new_dName.BL_name)
-                if ((long_dName > const_maxName) or (long_dName < const_minName)):
-                    return False
-                else:
-                    aName.BL_name = new_dName
+        
+        typenew_d   = (type(new_description) == str) 
+        type_d      = (type(description) == str)
+        if (type_d and typenew_d):
+            long_d = const_minDescription <= len(new_d.BL_description) <= const_maxDescription
+            if long_d:
+                aDescription = self.findDescription(description)
+                if (aDescription != []):
+                    new_d = clsBackLog.query.filter_by(BL_description = description).first()
+                    new_d.BL_description = new_description
                     db.session.commit()
                     return True
+        return False
 
-    def modifyName(self, Name, new_description):   
-        '''Permite actualizar los valores de una Descripcion'''    
-        if ((type(new_description) != str) or (type(Name) != str)):
-            return False
-        else:
-            aName = self.findName(Name)
-            if (aName == []):
-                return False
-            else:
-                new_mod = clsBackLog(BL_name = Name, BL_description = new_description)
-                long_description = len(new_mod.BL_description)
-                if ((long_description > const_maxDescription) or (long_description < const_minDescription)):
-                    return False
-                else:
-                    aName.BL_description = new_description
-                    db.session.commit()
-                    return True
-
-    
-    def deleteProduct(self, Name):
+    def deleteProduct(self, description):
         '''Permite eliminar una a descripción de la tabla'''
-        if (type(Name) != str):
+        if (type(description) != str):
             return False
         else:
-            long_Name = len(Name)
-            if ((long_Name > const_maxName)or(long_Name < const_minName)):
+            long_description =  (const_mindescription > len(description) > const_maxdescription)
+            if long_description:
                 return False
             else:
-                aName = self.findName(Name)
-                if (aName == []):
+                adescription = self.finddescription(description)
+                if (adescription == []):
                     return False
                 else:
-                    for i in aName:    
-                        db.session.delete(i)
-                        db.session.commit()
+                    tupla = clsBackLog.query.filter_by(BL_description = description).first()    
+                    db.session.delete(tupla)
+                    db.session.commit()
                     return True
 
     def actorsAsociatedToProduct(self,productId):
