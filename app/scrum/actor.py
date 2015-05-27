c42df3a851b323e9a20398365d8ee1f70cc0cd07
josema19 <josemacloud@gmail.com>
2015-05-27 17:19:05 -0430

    Permite modificar el nombre y/o descripción de los actores

--- app/scrum/actor.py
+++ app/scrum/actor.py
@@ -37,13 +37,20 @@ def AModifActor():
     #POST/PUT parameters
     params = request.get_json()
     results = [{'label':'/VProducto', 'msg':['Actor actualizado']}, {'label':'/VActor', 'msg':['Error al modificar actor']}, ]
-   
     idPila   = 1
+    
+    # Action code goes here
+    print(params)
+    idActor     = params['idActor'] #Obetenemos el id del actor
     newNameRole = params['nombre']
     newDescRole = params['descripcion'] 
     
+    actorNombre   = clsRole.query.filter_by(idrole = idActor).first() #Conseguimos el actor a modificar
+    
     oRole  = role()
-    result = oRole.updateRole(newNameRole,newNameRole,newDescRole,idPila)
+    print('nombre:%r nuevoNombre:%r nuevaDesc:%r',actorNombre.namerole, newNameRole, newDescRole)
+    result = oRole.updateRole(actorNombre.namerole, newNameRole, newDescRole)    #Modfificamos el actor deseado
+    
     if result:
         res = results[0]
         res['label'] = res['label'] + '/' + str(idPila)
@@ -65,8 +72,12 @@ def VActor():
     if "actor" in session:
         res['actor']=session['actor']
     #Action code goes here, res should be a JSON structure
+    idActor = request.args.get('idActor')
 
+    result   = clsRole.query.filter_by(idrole = idActor).first()
+    
     res['idPila'] = 1 
+    res['fActor'] = {'idActor':idActor, 'nombre':result.namerole, 'descripcion':result.roledescription}    
 
     #Action code ends here
     return json.dumps(res)
