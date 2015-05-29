2c8df748b31ed8c022abc9a050a462538968a543
oskcolorado <oskcolorado@gmail.com>
2015-05-29 00:43:55 -0430

    Agregados casos de prueba UserHistory.

--- testing/historyDummy.py
+++ testing/historyDummy.py
@@ -17,26 +17,27 @@ class userHistory(object):
     def insertUserHistory(self,cod_userHistory,id_History,type_accion,id_Accion,id_backLog):
         '''Permite insertar una Historia de usuario'''
         checkCodUserHistory    = type(cod_userHistory) == str
-        checkLenCodUserHistory = const_min_cod <= len(cod_userHistory) <= const_max_cod
-        checkIdHistory         = type(id_History) == int and id_History >= const_min_idHist
-        
-        if checkCodUserHistory and checkLenCodUserHistory and checkIdHistory:
-            oUserHistory = clsUserHistory.query.filter_by(id_userHistory = id_History).all()
+        if checkCodUserHistory :
+            checkLenCodUserHistory = const_min_cod <= len(cod_userHistory) <= const_max_cod
+            checkIdHistory         = type(id_History) == int and id_History >= const_min_idHist
             
-            if oUserHistory !=[] or id_History == 0:
-                checkTypeAccion = type_accion in arrayType
-                checkIdAccion   = type(id_Accion) == int and id_Accion >= const_min_id
-                checkIdBacklog  = type(id_backLog) == int and id_backLog >= const_min_id
+            if checkCodUserHistory and checkLenCodUserHistory and checkIdHistory:
+                oUserHistory = clsUserHistory.query.filter_by(id_userHistory = id_History).all()
                 
-                if checkTypeAccion and checkIdAccion and checkIdBacklog:
-                    oHistorys = clsAccions.query.filter_by(idaccion = id_Accion).all()
-                    oBackLog = clsBackLog.query.filter_by(id_backLog = id_backLog).all()
-            
-                    if (oBackLog != [] and oHistorys != []):
-                        new_history = clsUserHistory(cod_userHistory = cod_userHistory,id_History = id_History,type_accion = type_accion,ref_idaccion = id_Accion,id_backLog = id_backLog)
-                        db.session.add(new_history)
-                        db.session.commit()
-                        return True
+                if oUserHistory !=[] or id_History == 0:
+                    checkTypeAccion = type_accion in arrayType
+                    checkIdAccion   = type(id_Accion) == int and id_Accion >= const_min_id
+                    checkIdBacklog  = type(id_backLog) == int and id_backLog >= const_min_id
+                    
+                    if checkTypeAccion and checkIdAccion and checkIdBacklog:
+                        oHistorys = clsAccions.query.filter_by(idaccion = id_Accion).all()
+                        oBackLog = clsBackLog.query.filter_by(id_backLog = id_backLog).all()
+                
+                        if (oBackLog != [] and oHistorys != []):
+                            new_history = clsUserHistory(cod_userHistory = cod_userHistory,id_History = id_History,type_accion = type_accion,ref_idaccion = id_Accion,id_backLog = id_backLog)
+                            db.session.add(new_history)
+                            db.session.commit()
+                            return True
         return False
     
         
