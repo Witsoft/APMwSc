aa02832d48ec86ce47bb4f63623a88b358682470
patriv <patty.vl91@gmail.com>
2015-06-22 10:16:19 -0430

    Se agregaron casos de prueba para la primera parte de la interfaz con selenium.

--- testing/testInterface.py
+++ testing/testInterface.py
@@ -1,46 +1,230 @@
 # -*- coding: utf-8 -*-
 
-import unittest
-from selenium import webdriver
+from selenium                       import webdriver
+from selenium.webdriver.common.by   import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from time import sleep
-class PythonOrgSearch(unittest.TestCase):
+from selenium.webdriver.support.ui  import Select
+from selenium.common.exceptions     import NoSuchElementException
+from selenium.common.exceptions     import NoAlertPresentException
+from time                           import sleep
 
+import unittest, time, re
+import sys
+
+# Ruta que permite utilizar el módulo user.py
+sys.path.append('../app/scrum')
+
+from backLog   import *
+from user      import *
+from role      import * 
+from accions   import *
+from objective import *
+
+
+class CasosPrueba(unittest.TestCase):
+    
     def setUp(self):
         self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(30)
+        self.base_url = "http://localhost:5000/"
+        self.verificationErrors = []
+        self.accept_next_alert = True
+    
+    def test_casos_prueba(self):
+        driver = self.driver
+        
+        # Eliminamos los datos de prueba introducidos
+        
+        oBacklog   = backlog()
+        oUser      = user()
+        oActor     = role()
+        oAccion    = accions()
+        oObjective = objective()
+        
+        result    = oBacklog.findName('Taxi Seguro')
+        idBacklog = result[0].BL_idBacklog
+        result    = oUser.deleteUser('usuario')
+        print('Eliminar usuario',result)
+        result    = oActor.deleteActor('Actor 1',idBacklog)
+        print('Eliminar Actor',result)
+        result    = oAccion.deleteAccion('Accion',idBacklog)
+        print('Eliminar Accion',result)
+        result    = oAccion.deleteAccion('Accion 1',idBacklog)
+        print('Eliminar Accion',result) 
+        result    = oObjective.deleteObjective('Objetivo2',idBacklog)
+        print('Eliminar obj',result)  
+        result    = oObjective.deleteObjective('Objetivo1',idBacklog)
+        print('Eliminar obj',result) 
+         
+              
+        # Casos de prueba para registrar un usuario en la aplicacion.
+        
+        driver.get(self.base_url + "/#/VRegistro")
+        driver.find_element_by_id("fUsuario_nombre").clear()
+        driver.find_element_by_id("fUsuario_nombre").send_keys("Usuario Prueba")
+        driver.find_element_by_id("fUsuario_usuario").clear()
+        driver.find_element_by_id("fUsuario_usuario").send_keys("usuario")
+        driver.find_element_by_id("fUsuario_clave").clear()
+        driver.find_element_by_id("fUsuario_clave").send_keys("1234.ABC")
+        driver.find_element_by_id("fUsuario_clave2").clear()
+        driver.find_element_by_id("fUsuario_clave2").send_keys("1234.ABC")
+        driver.find_element_by_id("fUsuario_correo").clear()
+        driver.find_element_by_id("fUsuario_correo").send_keys("usuarioP@gmail.com")
+        Select(driver.find_element_by_id("fUsuario_actorScrum")).select_by_visible_text("Dueño de producto")
+        sleep(2)
+        driver.find_element_by_xpath("//button[@type='submit']").click()
+        sleep(2)
+        
+        # Casos de prueba para el inicio de sesion de un usuario.
+        
+        driver.find_element_by_id("fLogin_usuario").clear()
+        driver.find_element_by_id("fLogin_usuario").send_keys("usuario")
+        driver.find_element_by_id("fLogin_clave").clear()
+        driver.find_element_by_id("fLogin_clave").send_keys("1234.ABC")
+        sleep(2)
+        driver.find_element_by_xpath("//button[@type='submit']").click()
+        sleep(2)
 
-    def testLogin(self):
+        # Casos de prueba ver datos de un producto.
+        sleep(2)
+        driver.find_element_by_link_text("Ver").click()
+        sleep(2)
         
-        driver = self.driver
+        # Caso de prueba para crear un actor.
+
+#         driver.find_element_by_link_text("+Actor").click()       
+#         driver.find_element_by_id("fActor_nombre").clear()
+#         driver.find_element_by_id("fActor_nombre").send_keys("Actor1")
+#         driver.find_element_by_name("html").click()
+#         driver.find_element_by_id("taHtmlElement").clear()
+#         driver.find_element_by_id("taHtmlElement").send_keys("Nuevo actor")
+#         sleep(2)
+#         driver.find_element_by_xpath("//button[@type='submit']").click()
+#         sleep(3)
+#         
+#         # Caso de prueba para Modificar actor.
+         
+#         driver.find_element_by_xpath("(//a[contains(text(),'Ver')])[4]").click()
+#         driver.find_element_by_name("html").click()
+#         driver.find_element_by_id("taHtmlElement").clear()
+#         driver.find_element_by_id("taHtmlElement").send_keys("Nuevo actor 1")
+#         sleep(1)
+#         driver.find_element_by_xpath("//button[@type='submit']").click()
+#         sleep(1)
+#         driver.find_element_by_link_text("Salir").click()
+#         sleep(1)
+#         
+
+#        driver.find_element_by_css_selector("button.navbar-toggle").click()
+
+        # Caso de prueba para crear una accion.
         
-        driver.get("http://localhost:5000/#/")
-        elem = driver.find_element_by_name("usuario")
-        elem.send_keys("user")
-        elem = driver.find_element_by_name("clave")
-        elem.send_keys("NewUser@t3st")
-        sleep(1)
-        elem.send_keys(Keys.RETURN)
+        driver.find_element_by_link_text("+Accion").click()
+        driver.find_element_by_id("fAccion_descripcion").clear()
+        driver.find_element_by_id("fAccion_descripcion").send_keys("Accion")
+        sleep(2)
+        driver.find_element_by_xpath("//button[@type='submit']").click()
+        sleep(2)
+          
+        # Caso de prueba para ver la accion.
+       # found    = oAccion.searchAccion('Accion',idBacklog)
+       # idAccion = found[0].AC_idAccion
+        driver.find_element_by_xpath("(//a[contains(text(),'Ver')])[6]").click()
+        sleep(3)
         
-    def testRegister(self):
-        driver = self.driver
+        # Caso de prueba para modificar la accion.
+        #driver.get(self.base_url + "/#/VAccion/"+str(idAccion))
+        driver.find_element_by_id("fAccion_descripcion").clear()
+        driver.find_element_by_id("fAccion_descripcion").send_keys("Accion 1")
+        sleep(2)
+        driver.find_element_by_xpath("//button[@type='submit']").click()
+        sleep(2)
+   #     driver.find_element_by_css_selector("button.navbar-toggle").click()
+   
+       # Caso de prueba para ver la accion
+        driver.find_element_by_xpath("(//a[contains(text(),'Ver')])[6]").click()
+        sleep(3)     
         
-        driver.get("http://localhost:5000/#/VRegistro")
-        elem1 = driver.find_element_by_name("nombre")
-        elem1.send_keys("usuario")
-        elem1 = driver.find_element_by_name("usuario")
-        elem1.send_keys("user")
-        elem1 = driver.find_element_by_name("clave")
-        elem1.send_keys("NewUser@t3st")
-        elem1 = driver.find_element_by_name("clave2")
-        elem1.send_keys("NewUser@t3st")
-        elem1 = driver.find_element_by_name("correo")
-        elem1.send_keys("usuario@gmail.com")
-        sleep(1)
-        elem1.send_keys(Keys.RETURN)
+        # Caso de prueba para eliminar accion
+        driver.find_element_by_link_text("-accion").click()
+        sleep(2)
+
+        # Caso de prueba para crear un objetivo.
+        driver.find_element_by_link_text("+Objetivo").click()
+        driver.find_element_by_id("fObjetivo_descripcion").clear()
+        driver.find_element_by_id("fObjetivo_descripcion").send_keys("Objetivo2")
+        sleep(2)
+        Select(driver.find_element_by_id("fObjetivo_transversal")).select_by_visible_text("No")
+        driver.find_element_by_xpath("//button[@type='submit']").click()
+        sleep(2)
         
+        # Caso de prueba para ver el Objetivo
+        driver.find_element_by_xpath("(//a[contains(text(),'Ver')])[8]").click()
+        sleep(3)
+        
+        # Caso de prueba para modificar el objetivo.
+        driver.find_element_by_id("fObjetivo_descripcion").clear()
+        driver.find_element_by_id("fObjetivo_descripcion").send_keys("Objetivo1")
+        sleep(2)
+        driver.find_element_by_xpath("//button[@type='submit']").click()
+        sleep(2)
+        
+        # Caso de prueba para ver el objetivo
+        driver.find_element_by_xpath("(//a[contains(text(),'Ver')])[8]").click()
+        sleep(3)     
+        
+        # Caso de prueba para eliminar el objetivo
+        driver.find_element_by_link_text("-objetivo").click()
+        sleep(2)
+        
+#        driver.find_element_by_css_selector("button.navbar-toggle").click()
+#        driver.find_element_by_link_text("Salir").click()
+
+
+        
+        # Eliminamos los datos de prueba introducidos
+        
+        oBacklog = backlog()
+        oUser    = user()
+        oActor   = role()
+        
+        
+        result    = oBacklog.findName('Taxi Seguro')
+        idBacklog = result[0].BL_idBacklog
+        result    = oUser.deleteUser('usuario')
+        print('Eliminar usuario',result)
+        result    = oActor.deleteActor('Actor 1',idBacklog)
+        print('Eliminar Actor',result)
+        result    = oAccion.deleteAccion('Accion 1',idBacklog)
+        print('Eliminar Accion',result)
+        
+        # Cerramos la aplicacion.
+
+    
+    def is_element_present(self, how, what):
+        try: self.driver.find_element(by=how, value=what)
+        except NoSuchElementException: return False
+        return True
+     
+    def is_alert_present(self):
+        try: self.driver.switch_to_alert()
+        except NoAlertPresentException: return False
+        return True
+    
+    def close_alert_and_get_its_text(self):
+        try:
+            alert = self.driver.switch_to_alert()
+            alert_text = alert.text
+            if self.accept_next_alert:
+                alert.accept()
+            else:
+                alert.dismiss()
+            return alert_text
+        finally: self.accept_next_alert = True
+    
     def tearDown(self):
-        self.driver.close()
+        self.driver.quit()
+        self.assertEqual([], self.verificationErrors)
 
 if __name__ == "__main__":
     unittest.main()
